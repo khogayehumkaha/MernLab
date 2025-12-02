b0d5786eea623ed534670bdb0c095105a11acceb
alonecoderpravee <praveen.shetty@nitte.edu.in>
2025-12-02 07:21:24 +0530

    Added Query in pgdb.js

--- ISELab-Programs/pgdb.js
+++ ISELab-Programs/pgdb.js
@@ -9,10 +9,23 @@ database: 'MyDB',
 });
 module.exports=pool;
 
-/*  PgAdmin - Query-Tool
+/*  PgAdmin - Query-Tool 7.
 
 drop table users;
 
 create table users( id serial primary key, name varchar(50) not null,email varchar(50) not null unique );
+
+
+
+For Sign IN 10.JWT 
+
+CREATE TABLE clients (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+-- Insert a client
+INSERT INTO clients (username, password) VALUES ('student', 'student123');
 
 */
